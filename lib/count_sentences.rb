309d77a6e4051c95c25840db327892a6eb54574e
Lydia B <lydiabaik@gmail.com>
2020-01-10 03:48:27 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")? true : false
   end
 
   def question?
+    self.end_with?("?")? true : false
 
   end
 
   def exclamation?
-
+    self.end_with?("!")? true : false
   end
 
   def count_sentences
+    binding.pry
+    self.split(".")
+    self.split("!")
+    self.split("?")
+    #turns sentences into an array of sentences
+    self.count
 
   end
 end
